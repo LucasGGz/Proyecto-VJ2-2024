3c54e6a4f5b4eeb148aad8c7efccde6b5323333f
Casandra Reina <casandramailen.0010@gmail.com>
2024-01-22 05:29:48 -0300

    Cambios de setActive (solo funciona para el owner) aun no se como hacer para los demas players

---
+++ Assets/Scripts/ActivarObjeto.cs
@@ -0,0 +1,68 @@
+using UnityEngine;
+using Unity.Netcode;
+
+public class ActivarObjeto : NetworkBehaviour
+{
+    [SerializeField] private GameObject spawnObjectPrefab; // Cambié de Transform a GameObject
+
+    private GameObject spawnedObject; // Variable para almacenar la instancia del objeto
+
+    private void Awake()
+    {
+        // Al inicio del juego, asegúrate de que el objeto esté desactivado
+        if (IsServer)
+        {
+            spawnedObject = Instantiate(spawnObjectPrefab, transform.position, Quaternion.identity);
+            spawnedObject.GetComponent<NetworkObject>().Spawn();
+            spawnedObject.SetActive(false);
+        }
+    }
+
+    [ServerRpc]
+    private void InstantiaServerRpc()
+    {
+        // Si ya existe una instancia del objeto, simplemente actívalo en el servidor
+        if (spawnedObject != null)
+        {
+            spawnedObject.SetActive(true);
+        }
+        else
+        {
+            // Instancia el objeto en la posición actual del objeto que tiene este script en el servidor
+            spawnedObject = Instantiate(spawnObjectPrefab, transform.position, Quaternion.identity);
+            
+            // Sincroniza la instanciación en la red
+            spawnedObject.GetComponent<NetworkObject>().Spawn();
+            
+            // Puedes realizar más acciones aquí si es necesario, como modificar propiedades del spawnedObject
+        }
+    }
+
+    [ServerRpc]
+    private void DesactivarServerRpc()
+    {
+        // Desactiva el objeto cuando deja de haber trigger en el servidor
+        if (spawnedObject != null)
+        {
+            spawnedObject.SetActive(false);
+        }
+    }
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.tag == "Punto")
+        {
+            Debug.Log("Choco");
+            InstantiaServerRpc();
+        }
+    }
+
+    private void OnTriggerExit(Collider collider)
+    {
+        if (collider.gameObject.tag == "Punto")
+        {
+            Debug.Log("FUE");
+            DesactivarServerRpc();
+        }
+    }
+}
